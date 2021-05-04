944444b02989cff144e3b7e16591779ab6114a63
Jeong Arm <arm@tsnlab.com>
2021-05-04 15:06:44 +0900

    Refactor example

--- examples/icmp_echo/main.c
+++ examples/icmp_echo/main.c
@@ -6,55 +6,62 @@
 #include <pv/net/ipv4.h>
 #include <pv/net/arp.h>
 
-uint64_t mac;
-uint32_t ipv4;
+uint64_t my_mac;
+uint32_t my_ipv4;
 
-int process_arp(struct pv_ethernet* ether) {
-    // Set dst
-    ether->dmac = ether->smac;
-    ether->smac = mac;
+int process_arp(struct pv_arp* arp) {
+    if (arp->opcode != PV_ARP_OPCODE_ARP_REQUEST) {
+        printf("ARP opcode %d is not supported\n", arp->opcode);
+        return 0;
+    }
 
-    struct pv_arp* arp = (struct pv_arp*)PV_ETH_PAYLOAD(ether);
-	if (arp->opcode != PV_ARP_OPCODE_ARP_REQUEST) {
-		printf("ARP opcode %d is not supported\n", arp->opcode);
-		return 0;
-	}
+    if (arp->dst_proto != my_ipv4) {
+        // Not my IP, ignore
+        printf("Not my ip. ignore\n");
+        return 0;
+    }
 
-	if (arp->dst_proto != ipv4) {
-		// Not my IP, ignore
-		printf("Not my ip. ignore\n");
-		return 0;
-	}
+    printf("Reply to ARP request\n");
 
-	printf("Reply to ARP request\n");
+    arp->opcode = PV_ARP_OPCODE_ARP_REPLY;
+    arp->dst_hw = arp->src_hw;
+    arp->dst_proto = arp->src_proto;
+    arp->src_hw = my_mac;
+    arp->src_proto = my_ipv4;
 
-	arp->opcode = PV_ARP_OPCODE_ARP_REPLY;
-	arp->dst_hw = arp->src_hw;
-	arp->dst_proto = arp->src_proto;
-	arp->src_hw = mac;
-	arp->src_proto = ipv4;
-	
-	return 0;
+    return 0;
 }
 
-int process_ipv4(struct pv_ethernet* ether) {
-	//TODO
-	return 0;
+int process_ipv4(struct pv_ipv4* ipv4) {
+    void* payload = PV_IPv4_DATA(ipv4);
+
+    ipv4->dst = ipv4->src;
+    ipv4->src = my_ipv4;
+
+    switch (ipv4->proto) {
+    case PV_IP_PROTO_ICMP:
+        process_icmp(payload);
+    }
+    return 0;
+}
+
 }
 
 int process(struct pv_packet* packet) {
     struct pv_ethernet* ether = (struct pv_ethernet*)packet->payload;
-    // ether->dmac = ether->smac;
-    // ether->smac = mac;
+    ether->dmac = ether->smac;
+    ether->smac = my_mac;
 
-	printf("Got packet.\n");
+    printf("Got packet.\n");
+
+    void* payload = PV_ETH_PAYLOAD(ether);
 
     switch (ether->type) {
     case PV_ETH_TYPE_ARP:
-        process_arp(ether);
+        process_arp(payload);
         break;
     case PV_ETH_TYPE_IPv4:
-        process_ipv4(ether);
+        process_ipv4(payload);
         break;
     }
 
@@ -68,15 +75,15 @@ int main(int argc, char** argv) {
         return ret;
     }
 
-    pv_nic_get_mac(0, &mac);
-    printf("mac: %012lx\n", mac);
+    pv_nic_get_mac(0, &my_mac);
+    printf("mac: %012lx\n", my_mac);
 
-	pv_nic_get_ipv4(0, &ipv4);
-	printf("ipv4: %d.%d.%d.%d\n",
-	    ipv4 >> (8 * 3) & 0xff,
-		ipv4 >> (8 * 2) & 0xff,
-		ipv4 >> (8 * 1) & 0xff,
-		ipv4 >> (8 * 0) & 0xff);
+    pv_nic_get_ipv4(0, &my_ipv4);
+    printf("ipv4: %d.%d.%d.%d\n",
+        my_ipv4 >> (8 * 3) & 0xff,
+        my_ipv4 >> (8 * 2) & 0xff,
+        my_ipv4 >> (8 * 1) & 0xff,
+        my_ipv4 >> (8 * 0) & 0xff);
 
     struct pv_packet* pkt_buf[1024] = {};
 
