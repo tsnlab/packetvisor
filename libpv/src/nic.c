82679b52ffa67fec841e1b2c8ac6ccc2ab09f603
Jeong Arm <arm@tsnlab.com>
2021-05-13 16:31:34 +0900

    Use vlan_ids to store filter ids

--- libpv/src/nic.c
+++ libpv/src/nic.c
@@ -51,6 +51,9 @@ int pv_nic_init(uint16_t nic_count, struct rte_mempool* mbuf_pool) {
 }
 
 void pv_nic_finalize(void) {
+	for(int i = 1; i < nics_count; i += 1) {
+		pv_list_destroy(nics[i].vlan_ids);
+	}
 	free(nics);
 }
 
@@ -320,11 +323,19 @@ uint16_t pv_nic_tx_burst(uint16_t nic_id, uint16_t queue_id, struct pv_packet* p
 }
 
 bool pv_nic_vlan_filter_on(uint16_t nic_id, uint16_t id) {
+	pv_list_append(nics[nic_id].vlan_ids, &id);
 	int res = rte_eth_dev_vlan_filter(nics[nic_id].dpdk_port_id, id, 1);
 	return res == 0;
 }
 
 bool pv_nic_vlan_filter_off(uint16_t nic_id, uint16_t id) {
+	struct pv_list* const vlan_ids = nics[nic_id].vlan_ids;
+	for(int i = vlan_ids->current; i >= 0; i -= 1) {
+		uint16_t id_ = *(uint16_t*)pv_list_get(vlan_ids, i);
+		if(id_ == id) {
+			pv_list_del(vlan_ids, i);
+		}
+	}
 	int res = rte_eth_dev_vlan_filter(nics[nic_id].dpdk_port_id, id, 0);
 	return res == 0;
 }
