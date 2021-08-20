9695b00131841829d5a292f6524f35b9a77585e9
Jeong Arm <kjwonmail@gmail.com>
2021-08-20 15:42:58 +0900

    Fix typo on lock

--- src/thread.c
+++ src/thread.c
@@ -4,22 +4,22 @@
 #include <rte_rwlock.h>
 
 void pv_thread_lock_init(struct pv_thread_lock* lock) {
-    rte_rwlock_init((rte_rwlock_t*)&lock);
+    rte_rwlock_init((rte_rwlock_t*)lock);
 }
 void pv_thread_lock_read_lock(struct pv_thread_lock* lock) {
-    rte_rwlock_read_lock((rte_rwlock_t*)&lock);
+    rte_rwlock_read_lock((rte_rwlock_t*)lock);
 }
 
 void pv_thread_lock_read_unlock(struct pv_thread_lock* lock) {
-    rte_rwlock_read_unlock((rte_rwlock_t*)&lock);
+    rte_rwlock_read_unlock((rte_rwlock_t*)lock);
 }
 
 void pv_thread_lock_write_lock(struct pv_thread_lock* lock) {
-    rte_rwlock_write_lock((rte_rwlock_t*)&lock);
+    rte_rwlock_write_lock((rte_rwlock_t*)lock);
 }
 
 void pv_thread_lock_write_unlock(struct pv_thread_lock* lock) {
-    rte_rwlock_write_unlock((rte_rwlock_t*)&lock);
+    rte_rwlock_write_unlock((rte_rwlock_t*)lock);
 }
 
 int pv_thread_run_all(int main_function(void*), void* arg, bool call_main) {
