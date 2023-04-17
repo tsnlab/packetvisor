29fa2ae610ad6b4d851662034cb6a3e99416a033
Sik Yoon <sik@tsnlab.com>
2023-04-17 15:19:29 +0900

    Change path for compile and make

--- build.sh
+++ build.sh
@@ -4,22 +4,22 @@ echo "update submodule"
 git submodule update --init --recursive
 
 echo "compile bpftool and copy it"
-make -C ../bpftool/src
-cp ../bpftool/src/bpftool /usr/sbin
+make -C bpftool/src
+cp bpftool/src/bpftool /usr/sbin
 
 echo "compile xdp-tools libraries and utilities"
-make -C ../xdp-tools
+make -C xdp-tools
 
 echo "compile userspace application"
 clang -Wall -O2 -g \
-    -I../xdp-tools/lib/libbpf/src \
-    -I../xdp-tools/lib/libxdp/ \
-    -I../xdp-tools/lib/util \
-    -I../xdp-tools/headers/ \
-    -I../xdp-tools/lib/libbpf/src/root/usr/include \
-    -L../xdp-tools/lib/libbpf/src \
-    -L../xdp-tools/lib/libxdp/ \
-    -o pv \
-    main.c arp.c pv.c \
+    -Ixdp-tools/lib/libbpf/src \
+    -Ixdp-tools/lib/libxdp/ \
+    -Ixdp-tools/lib/util \
+    -Ixdp-tools/headers/ \
+    -Ixdp-tools/lib/libbpf/src/root/usr/include \
+    -Lxdp-tools/lib/libbpf/src \
+    -Lxdp-tools/lib/libxdp/ \
+    -o src/pv \
+    src/main.c src/arp.c src/pv.c \
     -l:libxdp.a -l:libbpf.a -lelf -lz
 echo "done"
