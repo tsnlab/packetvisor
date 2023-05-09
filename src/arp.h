b96f6784f0eaee7b37c11473b7f89179177f1510
Sik Yoon <sik@tsnlab.com>
2023-05-09 18:25:29 +0900

    Delete files releated to C

--- src/arp.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <net/ethernet.h>
-#include "pv.h"
-
-#define ARP_RSPN_PACKET_SIZE   42
-
-void gen_arp_response_packet(struct pv_packet* packet, const struct ether_addr smac);
-void gen_fixed_packet(struct pv_packet* packet);
