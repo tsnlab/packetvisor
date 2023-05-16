ba27a9f55062ad96c6db9ed5ea9f71a190c1a0a4
Sik Yoon <sik@tsnlab.com>
2023-05-16 11:16:33 +0900

    Move script

--- build.rs
+++ build.rs
@@ -4,6 +4,18 @@ use std::process;
 
 fn main() {
     let src_dir = path::PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+
+    let git_submodule_update_status = process::Command::new("git")
+        .arg("submodule")
+        .arg("update")
+        .arg("--init")
+        .arg("--recursive")
+        .current_dir(&src_dir)
+        .status()
+        .expect("could not complete submodule update");
+
+    assert!(git_submodule_update_status.success(), "submodule update failed");
+
     let bpftool_dir = src_dir.join("bpftool/src");
     let xdptools_dir = src_dir.join("xdp-tools");
     let libxdp_dir = xdptools_dir.join("lib/libxdp");
@@ -12,9 +24,9 @@ fn main() {
     let libbpf_src_dir = xdptools_dir.join("lib/libbpf/src");
 
     let status = process::Command::new("make")
-    .current_dir(&bpftool_dir)
-    .status()
-    .expect("could not execute make bpftool");
+        .current_dir(&bpftool_dir)
+        .status()
+        .expect("could not execute make bpftool");
 
     assert!(status.success(), "make failed");
 
--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export PATH=$PATH:$(dirname $(realpath $0))/bpftool/src
+#export PATH=$PATH:$(dirname $(realpath $0))/bpftool/src
 
 set -eo pipefail
 
-echo "update submodules of PV"
-git submodule update --init --recursive
+#echo "update submodules of PV"
+#git submodule update --init --recursive
 
-echo "Build packetvisor"
-cargo build --release
+#echo "Build packetvisor"
+#cargo build --release
 
 echo "Apply capabilities to run as non-root"
 sudo setcap CAP_SYS_ADMIN,CAP_NET_ADMIN,CAP_NET_RAW,CAP_DAC_OVERRIDE+ep target/release/pv3_rust
