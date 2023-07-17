97668d103e4de8dc188a8946c27df5395b77594a
junppyo <63575184+junppyo@users.noreply.github.com>
2023-07-17 15:00:05 +0900

    Fix get argument bug (#60)
    * Fix get argument bug
    
    * restore the value names

--- examples/echo.rs
+++ examples/echo.rs
@@ -28,32 +28,32 @@ fn main() {
     let matches = Command::new("echo")
         .arg(arg!(interface: -i --interface <interface> "Interface to use").required(true))
         .arg(
-            arg!(chunk_size: -s --chunk-size <size> "Chunk size")
+            arg!(chunk_size: -s --"chunk-size" <size> "Chunk size")
                 .required(false)
                 .default_value("2048"),
         )
         .arg(
-            arg!(chunk_count: -c --chunk-count <count> "Chunk count")
+            arg!(chunk_count: -c --"chunk-count" <count> "Chunk count")
                 .required(false)
                 .default_value("1024"),
         )
         .arg(
-            arg!(rx_ring_size: -r --rx-ring <count> "Rx ring size")
+            arg!(rx_ring_size: -r --"rx-ring" <count> "Rx ring size")
                 .required(false)
                 .default_value("64"),
         )
         .arg(
-            arg!(tx_ring_size: -t --tx-ring <count> "Tx ring size")
+            arg!(tx_ring_size: -t --"tx-ring" <count> "Tx ring size")
                 .required(false)
                 .default_value("64"),
         )
         .arg(
-            arg!(fill_ring_size: -f --fill-ring <count> "Fill ring size")
+            arg!(fill_ring_size: -f --"fill-ring" <count> "Fill ring size")
                 .required(false)
                 .default_value("64"),
         )
         .arg(
-            arg!(completion_ring_size: -o --completion-ring <count> "Completion ring size")
+            arg!(completion_ring_size: -o --"completion-ring" <count> "Completion ring size")
                 .required(false)
                 .default_value("64"),
         )
