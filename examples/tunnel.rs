d51ee472e3dd2e907124f52d46b2a23efc9efb15
SeongMinJin <sorksoek2@gmail.com>
2023-08-31 13:32:24 +0900

    Implement simple UDP capsulation

--- examples/tunnel.rs
+++ examples/tunnel.rs
@@ -1,32 +1,19 @@
 use clap::{arg, Command};
-use pnet::{
-    packet::{
-        arp::MutableArpPacket,
-        ethernet::{EtherTypes, MutableEthernetPacket},
-        ip::IpNextHeaderProtocols,
-        ipv4::MutableIpv4Packet,
-        udp::{MutableUdpPacket, UdpPacket},
-        MutablePacket,
-    },
-    util::MacAddr,
-};
-use std::{io, str::FromStr, thread, time::Duration};
-
-use std::net::UdpSocket;
+use std::{net::UdpSocket, io};
 
 fn main() {
     let matches = Command::new("tunnel")
-        .arg(arg!(gateway: -g --gateway <gateway> "Gateway interface").required(true))
-        .arg(arg!(tunnel: -t --tunnel <tunnel> "Tunnel interface").required(true))
+        .arg(arg!(interface: -i --interface <interface> "Interface"))
+        .arg(arg!(source: -s --source <source> "Source IP address"))
         .arg(
-            arg!(destination: -d --destination <destination> "Destination ip address")
+            arg!(destination: -d --destination <destination> "Destination IP address")
                 .required(true),
         )
         .get_matches();
 
-    let gateway = matches.get_one::<String>("gateway").unwrap();
-    let tunnel = matches.get_one::<String>("tunnel").unwrap();
-    let destination = matches.get_one::<String>("tunnel").unwrap();
+    let interface = matches.get_one::<String>("interface").unwrap();
+    let source = matches.get_one::<String>("source").unwrap();
+    let destination = matches.get_one::<String>("destination").unwrap();
 
     const CHUNK_SIZE: usize = 2048;
     const CHUNK_COUNT: usize = 1024;
@@ -35,8 +22,8 @@ fn main() {
     const FILLING_RING_SIZE: u32 = 64;
     const COMPLETION_RING_SIZE: u32 = 64;
 
-    let mut gateway = pv::NIC::new(&gateway, CHUNK_SIZE, CHUNK_COUNT).unwrap();
-    match gateway.open(
+    let mut interface = pv::NIC::new(&interface, CHUNK_SIZE, CHUNK_COUNT).unwrap();
+    match interface.open(
         RX_RING_SIZE,
         TX_RING_SIZE,
         FILLING_RING_SIZE,
@@ -45,96 +32,37 @@ fn main() {
         Ok(_) => {}
         Err(error) => println!("{error}"),
     }
-    let mut tunnel = pv::NIC::new(&tunnel, CHUNK_SIZE, CHUNK_COUNT).unwrap();
-    match tunnel.open(
-        RX_RING_SIZE,
-        TX_RING_SIZE,
-        FILLING_RING_SIZE,
-        COMPLETION_RING_SIZE,
-    ) {
-        Ok(_) => {}
-        Err(error) => println!("{error}"),
-    }
+    let socket = UdpSocket::bind(source).unwrap();
+    socket
+        .set_nonblocking(true)
+        .expect("Socket Nonblocking Error");
+
+    const RX_BATCH_SIZE: u32 = 64;
+    let mut packets: Vec<pv::Packet> = Vec::with_capacity(RX_BATCH_SIZE as usize);
+    let mut buf = [0; 1500];
 
     loop {
-        to_outside(&mut gateway, &mut tunnel);
-        to_inside(&mut tunnel, &mut gateway);
-    }
-
-}
-
-fn to_outside(from: &mut pv::NIC, to: &mut pv::NIC) {
-    const RX_BATCH_SIZE: u32 = 64;
-    let mut packets1: Vec<pv::Packet> = Vec::with_capacity(RX_BATCH_SIZE as usize);
-    let mut packets2: Vec<pv::Packet> = Vec::with_capacity(RX_BATCH_SIZE as usize);
-
-    let received = from.receive(&mut packets1);
-    println!("received:{received}");
-
-    if received > 0 {
-        for packet in &mut packets1 {
-            packet.dump();
-        }
-    } else {
-        thread::sleep(Duration::from_millis(100));
-    }
-}
-
-fn to_inside(from: &mut pv::NIC, to: &mut pv::NIC) {
-
-}
-
-
-
-
-fn forward(from: &mut pv::NIC, to: &mut pv::NIC) {
-    const RX_BATCH_SIZE: u32 = 64;
-    let mut packets1: Vec<pv::Packet> = Vec::with_capacity(RX_BATCH_SIZE as usize);
-    let mut packets2: Vec<pv::Packet> = Vec::with_capacity(RX_BATCH_SIZE as usize);
-
-    let received = from.receive(&mut packets1);
-
-    if received > 0 {
-        for packet in &mut packets1 {
-            match process_packet(from, packet) {
-                true => {
-                    packets2.push(to.copy_from(packet).unwrap());
-                }
-                false => {}
+        // Listening for interface
+        let recieved = interface.receive(&mut packets);
+        if recieved > 0 {
+            for i in 0..recieved as usize {
+                // send received packet to destination socket
+                socket.send_to(packets[i].get_buffer_mut(), destination).expect("Socket send Error");
             }
         }
-        to.send(&mut packets2);
-    } else {
-        thread::sleep(Duration::from_millis(100));
-    }
-}
 
-fn process_packet(nic: &mut pv::NIC, packet: &mut pv::Packet) -> bool {
-    let buffer = packet.get_buffer_mut();
-
-    let mut eth = MutableEthernetPacket::new(buffer).unwrap();
-    match eth.get_ethertype() {
-        EtherTypes::Ipv4 => {
-            let mut ipv4 = MutableIpv4Packet::new(eth.payload_mut()).unwrap();
-            match ipv4.get_next_level_protocol() {
-                IpNextHeaderProtocols::Udp => {
-                    let mut udp = MutableUdpPacket::new(ipv4.payload_mut()).unwrap();
-                    let payload = udp.payload_mut();
-                    let payload_len = payload.len();
-                    let payload_vec = payload.to_vec();
-                    packet.resize(payload_len);
-                    packet.replace_data(&payload_vec).unwrap();
-
-                    const RX_BATCH_SIZE: u32 = 64;
-                    let mut packets: Vec<pv::Packet> = Vec::with_capacity(RX_BATCH_SIZE as usize);
-                    packets.push(nic.copy_from(packet).unwrap());
-                    nic.send(&mut packets);
-
-                    false
-                }
-                _ => true,
-            }
+        // Listening for socket
+        match socket.recv_from(&mut buf) {
+            Ok(n) => {
+                // received data from socket and send to interface
+                let mut packet = interface.alloc().unwrap();
+                let data = buf.to_vec();
+                let mut packets: Vec<pv::Packet> = Vec::with_capacity(RX_BATCH_SIZE as usize);
+                packet.replace_data(&data).unwrap();
+                packets.push(interface.copy_from(&mut packet).unwrap());
+                interface.send(&mut packets);
+            },
+            Err(e) => panic!("encountered IO error: {e}"),
         }
-        _ => true,
     }
 }
