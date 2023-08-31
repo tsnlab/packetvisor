37fee99a1f0c651aeb3479867cfb8de23362dff9
SeongMinJin <sorksoek2@gmail.com>
2023-08-31 14:52:41 +0900

    Bundle example files

--- show_ip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-clear
-echo "--------client--------"
-sudo ip netns exec client ip -br a
-echo "--------host--------"
-sudo ip -br a
-echo "--------server--------"
-sudo ip netns exec server ip -br a
--- unset_tunnel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo ip link delete client_gateway
-sudo ip link delete client_tunnel
-sudo ip link delete server_gateway
-sudo ip netns delete client
-sudo ip netns delete server
